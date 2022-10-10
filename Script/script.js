523544ec16eb5eea540362ee180fff3c0575ff58
MarshallLiu-Dev <paulomartins12358@gmail.com>
2022-10-09 21:25:50 -0300

    first commit

---
+++ Script/script.js
@@ -0,0 +1,14 @@
+/* Exercício 13
+
+● Escreva um loop for que exibe números de 100 a 50 no console;
+for(int n = 1, n <=100; n++);
+
+var i = 50
+    while (i <= 100){
+        console.log('valor do loop:'+ i);
+        i++;
+    }*/
+
+    for(i=50; i<100 && i>0; i--){
+        console.log('o valor do loop é: ' + i);
+    }
